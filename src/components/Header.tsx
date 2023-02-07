56537535a9c176130751d384eada637ef9049e23
reinforest915820 <forest915820@gmail.com>
2023-02-06 19:10:04 -0500

    feat: YL-04 : add logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 function Header() {
     return (
-        <div className="p-10">
+        <div className="pt-7 px-14">
             <Navbar />
         </div>
     )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,9 @@ import React from "react";
 function Navbar() {
     return (
         <div className="flex">
-            Navbar
+            <div>
+                <img className="w-14" src="/logo.png" alt="Ylabs Logo"/>
+                </div> 
         </div>
     )
 }
