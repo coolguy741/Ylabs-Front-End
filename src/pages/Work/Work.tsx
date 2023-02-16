2fed99902f0748bcfaacffaf559660dcd36372f2
reinforest915820 <forest915820@gmail.com>
2023-02-16 11:32:02 -0500

    feat: complete the work mockup data

--- src/pages/Work/Work.tsx
+++ src/pages/Work/Work.tsx
@@ -1,4 +1,20 @@
-import React from 'react';
+const projectsData = [
+    {
+        image: "/images/work_Arufabudo.png",
+        name: "Arufabudo:",
+        desc: "Premium, ready-made and custom boxing gear." ,
+    },
+    {
+        image: "/images/work_Planifai.png",
+        name: "Planifai:",
+        desc: "Comprehensive business plans starting from $300." ,
+    },
+    {
+        image: "/images/work_Rapidcoder.png",
+        name: "Rapidcoder:",
+        desc: "Advanced computer science courses for an ever dynamic world." ,
+    },
+]
 
 const Work = () => {
     return (
