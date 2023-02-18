ff9db2f83612a54afc6bbc8ed40e2078e1f6bd1d
reinforest915820 <forest915820@gmail.com>
2023-02-18 08:01:04 -0500

    fix: toggleButton effect

--- src/components/atoms/ThemeToggle/ThemeToggle.tsx
+++ src/components/atoms/ThemeToggle/ThemeToggle.tsx
@@ -11,7 +11,7 @@ const ThemeToggle = () => {
 	}
 
 	return (
-		<div className={theme === 'light' ? "container white-theme" : "container dark-theme"}>
+		<div className={theme === 'light' ? "container white" : "container dark"}>
 			<label className="btn-color-mode-switch-theme">
 				<input type="checkbox" checked={theme === 'dark'} name="color_mode" id="color_mode" value="1" onClick={() => toggleTheme()}/>
 				<label htmlFor="color_mode" data-on="DE" data-off="EN"
