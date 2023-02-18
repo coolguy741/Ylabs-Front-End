2fe86367f89f7321c30d216c2f43ad15b3d3bf33
reinforest915820 <forest915820@gmail.com>
2023-02-18 08:50:14 -0500

    fix: toggleButton sun icon color

--- src/components/atoms/ThemeToggle/ThemeToggle.tsx
+++ src/components/atoms/ThemeToggle/ThemeToggle.tsx
@@ -15,7 +15,7 @@ const ThemeToggle = () => {
 			<label className="btn-color-mode-switch-theme">
 				<input type="checkbox" checked={theme === 'dark'} name="color_mode" id="color_mode" value="1" onClick={() => toggleTheme()}/>
 				<label htmlFor="color_mode" data-on="DE" data-off="EN"
-					className="btn-color-mode-switch-theme-inner"></label>
+					className="btn-color-mode-switch-theme-inner fill-current"></label>
 			</label>
 		</div>
 	);
