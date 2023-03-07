7e630aa5bec2329fa8a44e6472a6a5a1136a54ac
coolguy741 <111754563+coolguy741@users.noreply.github.com>
2023-03-07 17:26:27 -0500

    fix: deleted the unecessary OBJLoader

--- src/components/molecules/ThreeDLogo/ThreeDLogo.tsx
+++ src/components/molecules/ThreeDLogo/ThreeDLogo.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 import * as THREE from 'three';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+// import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 export interface ThreeDLogoProps {
