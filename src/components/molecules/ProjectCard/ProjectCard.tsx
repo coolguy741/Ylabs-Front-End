4469ff3900b2be1e37124fc78352cc3f2cae4292
coolguy741 <111754563+coolguy741@users.noreply.github.com>
2023-03-08 14:57:53 -0500

    fix: work page

--- src/components/molecules/ProjectCard/ProjectCard.tsx
+++ src/components/molecules/ProjectCard/ProjectCard.tsx
@@ -2,10 +2,9 @@ import Hr from "../../atoms/Hr/Hr";
 import { Link } from "react-router-dom";
 import ArrowIconDark from "../../../assets/icons/ArrowIconDark.png";
 import ArrowIconWhite from "../../../assets/icons/ArrowIconWhite.png";
-import { useEffect } from "react";
 import { useTheme } from "../../../context/ThemeProvider";
 
-const ProjectCard = ({image, icon, name, desc, link}:{image:string, icon:string, name:string, desc:string, link:string,
+const ProjectCard = ({image, icon, name, desc, link}:{image:string, icon:{exist: boolean, iconDark:string, iconWhite: string}, name:string, desc:string, link:string,
 }) => {
 	const {theme} = useTheme();
 
@@ -17,14 +16,14 @@ const ProjectCard = ({image, icon, name, desc, link}:{image:string, icon:string,
 					<Link to = {link}>
 						<img src={image} alt={name}/>
 					</Link>
-					{ icon === "" ? <></> :<img className="absolute -top-[60px] -right-[60px]" src="./icons/coming_soon_icon.png" alt={icon}/> }
+					{ icon.exist === false ? <></> :<img className="absolute -top-[60px] -right-[60px] w-[120px] y-[120px]" src={theme === "dark" ? icon.iconWhite : icon.iconDark} alt="coming soom icon"/> }
 				</div>
 				<div className="w-1/2 flex-col relative ml-16">
 					<div className="absolute bottom-0 w-full">
 						<h2 className="mb-6">{name}</h2>
 						<div className="flex justify-between w-full relative">
 							<p className="max-w-[503px] font-h3">{desc}</p>
-							<img className="w-[73px] h-[73px] absolute bottom-0 right-0" src={theme === "dark" ? ArrowIconWhite : ArrowIconDark} alt="coming soon icon"/>
+							<img className="w-[73px] h-[73px] absolute bottom-0 right-0" src={theme === "dark" ? ArrowIconWhite : ArrowIconDark} alt="Right Arrow"/>
 						</div>
 					</div>
 				</div>
--- src/pages/Work/Work.tsx
+++ src/pages/Work/Work.tsx
@@ -5,21 +5,33 @@ import Layout1 from "../../components/organisms/Layout1/Layout1";
 const projectsData = [
     {
         image: "/images/work_Arufabudo.png",
-        icon: "",
+        icon: {
+            exist : false,
+            iconDark: "",
+            iconWhite: "",
+        },
         link: "/arufo",
         name: "Arufabudo:",
         desc: "Manufacturing and selling premium boxing gear for warriors who aim to stand out from the crowd." ,
     },
     {
         image: "/images/work_Planifai.png",
-        icon: "",
+        icon: {
+            exist : false,
+            iconDark: "",
+            iconWhite: "",
+        },
         link: "/planify",
         name: "planify:",
         desc: "An AI-powered platform that creates comprehensive business plans in a matter of minutes." ,
     },
     {
         image: "/images/work_Rapidcoder.png",
-        icon: "/icons/coming_soon_icon.png",
+        icon: {
+            exist : true,
+            iconDark: "/icons/Coming_Soon_Badge_Dark.svg",
+            iconWhite: "/icons/Coming_Soon_Badge_White.svg",
+        },
         link: "/rapid",
         name: "Rapidcoder:",
         desc: "An advanced e-learning platform for programmers and developers who aspire to be the best in their fields." ,
